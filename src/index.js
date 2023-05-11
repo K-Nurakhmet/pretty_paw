2b18cb6cda5765968b89b6f13754c283332db0a6
K-Nurakhmet <k_nura_95@mail.ru>
2023-05-11 19:03:42 +0500

    all refresh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './index.html';
 // New modules //
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import './index.scss';
 
 
 // Use modules //
+
+const videoBG = document.querySelector('.video-bg');
+
+videoBG.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
